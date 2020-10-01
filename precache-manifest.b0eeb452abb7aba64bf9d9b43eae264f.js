699b3d2b2ffefda8d78eb12e772c56376165b5e6
akshay <akshay.gaikwad@vshift.com>
2020-10-01 18:24:01 +0530

    Updates

---
+++ precache-manifest.b0eeb452abb7aba64bf9d9b43eae264f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ae63f52ff3487b6f30561336895edb",
+    "url": "/mern-gh-page/index.html"
+  },
+  {
+    "revision": "94d7ac180d88f083dd1d",
+    "url": "/mern-gh-page/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "16ccebc341e555216c06",
+    "url": "/mern-gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94d7ac180d88f083dd1d",
+    "url": "/mern-gh-page/static/js/2.83a6a128.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mern-gh-page/static/js/2.83a6a128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ccebc341e555216c06",
+    "url": "/mern-gh-page/static/js/main.fa003394.chunk.js"
+  },
+  {
+    "revision": "cb294ee32fa346b62931",
+    "url": "/mern-gh-page/static/js/runtime-main.5e81f2e8.js"
+  }
+]);
